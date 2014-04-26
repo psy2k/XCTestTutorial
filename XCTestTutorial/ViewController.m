ec8f87f18c3e7d740d50c3856609a78aeb2f5f95
Nikos Maounis <nikospsy2k@gmail.com>
2014-04-26 15:19:57 +0300

    Basic tableView implementation
    Implemented a basic table for testing purposes

--- XCTestTutorial/ViewController.m
+++ XCTestTutorial/ViewController.m
@@ -10,20 +10,49 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 @end
 
 @implementation ViewController
 
+#pragma mark - View loading methods
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.title = @"XCTest Tutorial";
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark - Memory warning methods
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView delegate methods
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 15;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *reuseId = [NSString stringWithFormat:@"%ld/%ld",(long)indexPath.section,(long)indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseId];
+    if(!cell)
+    {
+        cell =[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseId];
+        cell.selectionStyle = UITableViewCellSelectionStyleGray;
+
+    }
+
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %ld", (long)indexPath.row+1];
+
+    return cell;
 }
 
 @end
