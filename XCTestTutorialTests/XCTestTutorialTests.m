f9d32cc1fed5bf5c8b4a7f13833c1544840936b5
Nikos Maounis <nikospsy2k@gmail.com>
2014-04-26 16:19:38 +0300

    Implemented tableView test cases
    Simple tests for testing UITableView and its cells

--- XCTestTutorialTests/XCTestTutorialTests.m
+++ XCTestTutorialTests/XCTestTutorialTests.m
@@ -31,12 +31,67 @@
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.vc = nil;
     [super tearDown];
 }
 
-- (void)testExample
+#pragma mark - View loading tests
+-(void)testThatViewLoads
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertNotNil(self.vc.view, @"View not initiated properly");
+}
+
+- (void)testParentViewHasTableViewSubview
+{
+    NSArray *subviews = self.vc.view.subviews;
+    XCTAssertTrue([subviews containsObject:self.vc.tableView], @"View does not have a table subview");
+}
+
+-(void)testThatTableViewLoads
+{
+    XCTAssertNotNil(self.vc.tableView, @"TableView not initiated");
+}
+
+#pragma mark - UITableView tests
+- (void)testThatViewConformsToUITableViewDataSource
+{
+    XCTAssertTrue([self.vc conformsToProtocol:@protocol(UITableViewDataSource) ], @"View does not conform to UITableView datasource protocol");
+}
+
+- (void)testThatTableViewHasDataSource
+{
+    XCTAssertNotNil(self.vc.tableView.dataSource, @"Table datasource cannot be nil");
+}
+
+- (void)testThatViewConformsToUITableViewDelegate
+{
+     XCTAssertTrue([self.vc conformsToProtocol:@protocol(UITableViewDelegate) ], @"View does not conform to UITableView delegate protocol");
+}
+
+- (void)testTableViewIsConnectedToDelegate
+{
+    XCTAssertNotNil(self.vc.tableView.delegate, @"Table delegate cannot be nil");
+}
+
+- (void)testTableViewNumberOfRowsInSection
+{
+    NSInteger expectedRows = 15;
+    XCTAssertTrue([self.vc tableView:self.vc.tableView numberOfRowsInSection:0]==expectedRows, @"Table has %ld rows but it should have %ld", (long)[self.vc tableView:self.vc.tableView numberOfRowsInSection:0], (long)expectedRows);
+}
+
+- (void)testTableViewHeightForRowAtIndexPath
+{
+    CGFloat expectedHeight = 44.0;
+    CGFloat actualHeight = self.vc.tableView.rowHeight;
+    XCTAssertEqual(expectedHeight, actualHeight, @"Cell should have %f height, but they have %f", expectedHeight, actualHeight);
+}
+
+- (void)testTableViewCellCreateCellsWithReuseIdentifier
+{
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    UITableViewCell *cell = [self.vc tableView:self.vc.tableView cellForRowAtIndexPath:indexPath];
+    NSString *expectedReuseIdentifier = [NSString stringWithFormat:@"%ld/%ld",(long)indexPath.section,(long)indexPath.row];
+    XCTAssertTrue([cell.reuseIdentifier isEqualToString:expectedReuseIdentifier], @"Table does not create reusable cells");
 }
 
 @end
